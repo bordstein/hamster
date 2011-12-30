7dc8e55d08494672571e23677a81ed794e99986a
Nurio <nurio@nurio.at>
2011-12-30 12:37:41 +0100

    added wrapper for couchdb

---
+++ playground/moviedb.py
@@ -0,0 +1,18 @@
+import couchdb
+
+class MovieDB(object):
+    def __init__(self, dbname):
+        couch = couchdb.Server()
+        self.db = couch[dbname]
+
+    def get_movie(self, id):
+        return self.db.get(id)
+
+    def get_movie_id_title_for_position(self, position):
+        vr = self.db.view("_design/test/_view/title", skip=position, limit=1)
+        result = vr.rows[0]
+        return (result.id, result.key)
+
+    def get_total_movies(self):
+        vr = self.db.view("_design/mv/_view/count")
+        return vr.rows[0].value
