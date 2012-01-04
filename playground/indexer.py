100f7956eafc3f6b50f784f08dff4a10d29f73cf
Nurio <nurio@nurio.at>
2012-01-04 13:17:18 +0100

    using multiple processes and writer server for indexing

--- playground/indexer.py
+++ playground/indexer.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python
 
-from gevent import monkey; monkey.patch_socket()
+#from gevent import monkey; monkey.patch_socket()
 from dbcloner import normalize
 
 import os
 import re
 import imdb
-import json
+#import json
 #import u1db
-import couchdb
-import gevent
-from gevent import pool
+#import couchdb
+#import gevent
+#from gevent import pool
+from multiprocessing import Pool
 from whoosh_action.index import HamsterIndex
+from indexwriter import QueueManager
 
-rex = re.compile(".*\[(\d{7})\]")
-imdb_db = imdb.IMDb()
-index = HamsterIndex("/tmp/hamster.idx")
-#db = u1db.open("/tmp/out.db", create=True)
-couch = couchdb.Server()
-db = couch['movies']
-
-g_pool = pool.Pool(size=25)
-global count
-count = 0
+def fetch_and_normalize(imdb_id):
+    print "fetching", imdb_id, "..."
+    imdb_db = imdb.IMDb()
+    movie = imdb_db.get_movie(imdb_id)
+    nm = normalize(movie)
+    #db.create_doc(json.dumps(nm), doc_id=imdb_id)
+    nm['_id'] = str(imdb_id)
+    #db.save(nm)
+    #index.index_movie(nm)
+    print "fetched!"
+    QueueManager.register('index')
+    m = QueueManager(address=('', 50000), authkey='abracadabra')
+    m.connect()
+    m.index(nm)
 
 def callback(arg, directory, files):
     for file in files:
         match = rex.match(file)
         if match:
             imdb_id = match.group(1)
-            g_pool.spawn(fetch_and_normalize, imdb_id)
-            gevent.sleep(0)
-
-def fetch_and_normalize(imdb_id):
-    print "fetching", imdb_id, "..."
-    movie = imdb_db.get_movie(imdb_id)
-    nm = normalize(movie)
-    #db.create_doc(json.dumps(nm), doc_id=imdb_id)
-    nm['_id'] = str(imdb_id)
-    #db.save(nm)
-    index.index_movie(nm)
-    print "fetched!"
+            #g_pool.spawn(fetch_and_normalize, imdb_id)
+            #gevent.sleep(0)
+            pool.apply_async(fetch_and_normalize, (imdb_id,))
 
 def counter(arg, directory, files):
     global count
@@ -49,6 +46,20 @@ def counter(arg, directory, files):
         if match:
             count+=1
 
+rex = re.compile(".*\[(\d{7})\]")
+imdb_db = imdb.IMDb()
+index = HamsterIndex("/tmp/hamster.idx")
+#db = u1db.open("/tmp/out.db", create=True)
+#couch = couchdb.Server()
+#db = couch['movies']
+
+#g_pool = pool.Pool(size=25)
+pool = Pool(8)
+global count
+count = 0
+
 os.path.walk("/media/DATA/media/movies/", callback, "secret message")
+pool.close()
+pool.join()
 #os.path.walk("/media/DATA/media/movies/", counter, "secret message")
 #print count
---
+++ playground/indexwriter.py
@@ -0,0 +1,28 @@
+from multiprocessing.managers import BaseManager
+from whoosh_action.index import HamsterIndex
+import u1db
+import json
+
+def index(movie):
+    try:
+        h_index.index_movie(movie)
+        #db.create_doc(json.dumps(movie), doc_id=movie["_id"])
+        print "indexed and stored:", movie['title']
+    except Exception as e:
+        print "failed"
+        print e
+
+class QueueManager(BaseManager):
+    pass
+
+if __name__ == "__main__":
+
+    h_index = HamsterIndex("/tmp/hamster.idx")
+    #db = u1db.open("/tmp/out.db", create=True)
+
+    QueueManager.register('index', callable=index)
+    m = QueueManager(address=('', 50000), authkey='abracadabra')
+    s = m.get_server()
+    s.serve_forever()
+
+    
