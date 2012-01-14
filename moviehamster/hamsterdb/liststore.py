c535b40c587a5e3f042a26732701b48f987e293e
Nurio <nurio@nurio.at>
2012-01-14 20:48:56 +0100

    listing lists implemented

--- moviehamster/hamsterdb/liststore.py
+++ moviehamster/hamsterdb/liststore.py
@@ -7,7 +7,29 @@ class ListStore(object):
         self.username = username
 
     def _create_index(self):
-        pass
+        indexes = db.list_indexes()
+        if "by-name" not in [idx[0] for idx in indexes]:
+            self.db.create_index("by-name", ['name'])
+        if "userlists-by-name" not in [idx[0] for idx in indexes]:
+            self.db.create_index("userlists-by-name", ['user', 'name'])
+
+    def list_user_lists(self):
+        results = self.db.get_from_index(
+        "userlists-by-name",             # name of index
+            [                            # begin the list of index keys
+                (self.username, "*", )   # an index key
+            ]
+        )
+        return _extract_doc_ids(results)
+
+    def list_all_lists(self):
+        results = self.db.get_from_index(
+        "by-name",                     # name of index
+            [                               # begin the list of index keys
+                ("*", )                  # an index key
+            ]
+        )
+        return _extract_doc_ids(results)
 
     def create(self, listname):
         ml = MovieList(self.username, listname)
@@ -28,12 +50,20 @@ class ListStore(object):
             ml = movielist_from_doc(doc)
             return ml
 
+def _extract_doc_ids(doclist):
+    retval = []
+    for doc in doclist:
+        retval.append(doc.doc_id)
+    return retval
+
 if __name__ == "__main__":
     import u1db
     db = u1db.open("/tmp/list.db", create=True)
     ls = ListStore(db, "user")
     #ls.create('blupp')
-    ml = ls.get_user_list("watched")
-    ml.append(2323)
-    ls.save(ml)
-    print ml
+    #ml = ls.get_user_list("watched")
+    #ml.append(2323)
+    #ls.save(ml)
+    #print ml
+    print ls.list_user_lists()
+    print ls.list_all_lists()
